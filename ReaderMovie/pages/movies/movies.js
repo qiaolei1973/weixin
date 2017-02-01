885a25b27e56d06bc67c3be6549783f19f6c37ab
乔磊 <qiaolei@qiaoleideMacBook-Air.local>
2017-02-01 17:03:05 +0800

    movie页面

--- ReaderMovie/pages/movies/movies.js
+++ ReaderMovie/pages/movies/movies.js
@@ -2,28 +2,58 @@ const {globalData: {doubanBase}} = getApp();
 
 Page({
     data: {
-        inTheaters: [],
-        comingSoon: [],
-        top250: []
+        inTheaters: {},
+        comingSoon: {},
+        top250: {}
     },
-    getMovieListData: function (url) {
-        return new Promise(function (resolve, reject) {
-            wx.request({
-                url,
-                method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-                header: {
-                    "Content-Type": "json"
-                }, // 设置请求的 header
-                success: function (res) {
-                    // success
-                    resolve({ [url]: res });
-                },
-                fail: function (err) {
-                    // fail
-                    reject(err);
-                },
-            })
+    getMovieListData: function (url, key) {
+        const {processData} = this;
+        // return new Promise(function (resolve, reject) {
+        wx.request({
+            url,
+            method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+            header: {
+                "Content-Type": "json"
+            }, // 设置请求的 header
+            success: (res) => {
+                // success
+                // resolve({ [url]: res });
+                const data = processData(res.data);
+                this.setData({
+                    [key]:data
+                })
+
+            },
+            fail: (err) => {
+                // fail
+                // reject(err);
+            },
+        }).bind(this);
+        // })
+    },
+
+    processData: function (data) {
+        const {title, subjects} = data;
+
+        let temp = {};
+        temp.title = title;
+
+        temp.subjects = subjects.map((item) => {
+            const {
+                id,
+                title,
+                rating: { average },
+                images: { large }
+            } = item;
+
+            return {
+                id,
+                title,
+                average,
+                image: large
+            };
         })
+        return temp;
     },
 
     onLoad: function (e) {
@@ -40,23 +70,40 @@ Page({
 
         const {getMovieListData} = this;
 
-        let
-            get_inTheaters = getMovieListData(inTheatersUrl),
-            get_comingSoon = getMovieListData(comingSoonUrl),
-            get_top250 = getMovieListData(top250Url);
+        getMovieListData(inTheatersUrl, 'inTheaters');
+        getMovieListData(comingSoonUrl, 'comingSoon');
+        getMovieListData(top250Url, 'top250');
 
+        // Promise.all([
+        //     getMovieListData(inTheatersUrl),
+        //     getMovieListData(comingSoonUrl),
+        //     getMovieListData(top250Url)
+        // ])
+        //     .then(res => {
+        //         let inTheaters, comingSoon, top250;
 
-        Promise.all([get_inTheaters, get_comingSoon, get_top250])
-            .then(res => {
-                let inTheaters, comingSoon, top250;
-                res.map((item) => {
-                    !inTheaters && (inTheaters = item[inTheatersUrl] && item[inTheatersUrl].data.subjects);
-                    !comingSoon && (comingSoon = item[comingSoonUrl] && item[comingSoonUrl].data.subjects);
-                    !top250 && (top250 = item[top250Url] && item[top250Url].data.subjects);
-                })
-                console.log(inTheaters, comingSoon, top250);
-            })
-            .catch(err => err);
+        //         res.map((item) => {
+        //             //
+        //             !inTheaters &&
+        //                 (inTheaters = item[inTheatersUrl] &&
+        //                     processData(item[inTheatersUrl].data));
+        //             //
+        //             !comingSoon &&
+        //                 (comingSoon = item[comingSoonUrl] &&
+        //                     processData(item[comingSoonUrl].data));
+        //             //
+        //             !top250 &&
+        //                 (top250 = item[top250Url] &&
+        //                     processData(item[top250Url].data));
+        //         })
+
+        //         this.setData({
+        //             inTheaters,
+        //             comingSoon,
+        //             top250
+        //         })
+        //     })
+        //     .catch(err => console.log(err));
 
     }
 })
